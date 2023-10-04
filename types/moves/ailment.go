e8bf3383bab21493660635256b1d69ec42536e54
Landon <landonp95@gmail.com>
2023-10-04 16:22:03 -0400

    document ailment type

--- types/moves/ailment.go
+++ types/moves/ailment.go
@@ -1,17 +1,33 @@
 package moves
 
 type Ailment struct {
-	ID    int `json:"id"`
+	// The unique identifier for the ailment.
+	ID int `json:"id"`
+
+	// Moves associated with this ailment.
 	Moves []struct {
+		// The name of the move.
 		Name string `json:"name"`
-		URL  string `json:"url"`
+
+		// The URL to access detailed information about the move.
+		URL string `json:"url"`
 	} `json:"moves"`
-	Name  string `json:"name"`
+
+	// The name of the ailment.
+	Name string `json:"name"`
+
+	// Names of the ailment in different languages.
 	Names []struct {
+		// Information about the language of the name.
 		Language struct {
+			// The name of the language.
 			Name string `json:"name"`
-			URL  string `json:"url"`
+
+			// The URL to access detailed information about the language.
+			URL string `json:"url"`
 		} `json:"language"`
+
+		// The name of the ailment in a specific language.
 		Name string `json:"name"`
 	} `json:"names"`
 }
